21bc25b3039e7c41f3643bee663b936216fb93d0
shinya09 <74222581+shinya09@users.noreply.github.com>
2021-02-22 10:34:51 +0900

    [delete] AB#1260 必要のないimportパッケージを削除する

--- my-app-typescript/src/isRWAN.ts
+++ my-app-typescript/src/isRWAN.ts
@@ -1,6 +1,5 @@
-import React from 'react'
 import axios from 'axios';
-import ipRangeCheck from 'ip-range-check'
+import ipRangeCheck from 'ip-range-check';
 
 export async function isRWAN(): Promise<boolean> {
     const response = await axios.get("http://ip-api.com/json");
