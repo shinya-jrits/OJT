28d6d04fcdf92526a649f87d1fdafd4dc2b071e7
shinya09 <74222581+shinya09@users.noreply.github.com>
2021-02-22 15:53:55 +0900

    [delete] AB#1260 不要なconsole.logを削除する

--- my-app-typescript/src/isRWAN.ts
+++ my-app-typescript/src/isRWAN.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import ipRangeCheck from 'ip-range-check';
 
 export async function isRWAN(): Promise<boolean> {
-    const response = await axios.get('https://ipinfo.io/json')
-    console.log(response);
+    const response = await axios.get('https://ipinfo.io/json');
     return ipRangeCheck(response.data.ip as string, "133.139.0.0/16");
 }
