c4470eb098a7ba394e6e09114aa276f4d403f0ad
shinya09 <74222581+shinya09@users.noreply.github.com>
2021-02-22 15:37:06 +0900

    [update] AB#1260 ipinfo.ioでAccessTokenを使わずにIP取得する

--- my-app-typescript/src/isRWAN.ts
+++ my-app-typescript/src/isRWAN.ts
@@ -2,9 +2,7 @@ import axios from 'axios';
 import ipRangeCheck from 'ip-range-check';
 
 export async function isRWAN(): Promise<boolean> {
-    if (process.env.REACT_APP_IPINFO_URL == null) {
-        throw new Error("IPinfo.ioのURLが指定されていません");
-    }
-    const response = await axios.get(process.env.REACT_APP_IPINFO_URL);
+    const response = await axios.get('https://ipinfo.io/json')
+    console.log(response);
     return ipRangeCheck(response.data.ip as string, "133.139.0.0/16");
 }
